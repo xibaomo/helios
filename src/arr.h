9affa517a2855061e994bcf330bac6477c9506cf
fxua <fxua@gmail.com>
2026-04-26 21:55:13 -0700

    add copy mvoe assignment

--- src/arr.h
+++ src/arr.h
@@ -6,13 +6,52 @@
 template <typename T>
 class Array1D {
  protected:
-  std::unique_ptr<T[]> m_data;
-  size_t m_size;
+  std::unique_ptr<T[]> m_data = nullptr;
+  size_t m_size = 0;
 
  public:
   Array1D(size_t s = 0) : m_data(nullptr), m_size(s) {
     resize(m_size);
     zero();
+  }
+
+  // copy constructor
+  Array1D(const Array1D<T>& other) {
+    m_size = other.m_size;
+    if (m_size == 0) return;
+
+    m_data = std::make_unique<T[]>(m_size);
+    std::copy(other.m_data.get(), other.m_data.get() + m_size, m_data.get());
+  }
+  // copy assignment
+  Array1D<T>& operator=(const Array1D<T>& other) {
+    if (m_data && m_size != other.m_size) {
+      m_data.reset();
+    }
+    m_size = other.m_size;
+    if (m_size > 0) {
+      m_data = std::make_unique<T[]>(m_size);
+      std::copy(other.m_data.get(), other.m_data.get() + m_size, m_data.get());
+    }
+    return *this;
+  }
+  // move constructor
+  Array1D(Array1D<T>&& other) {
+    m_data = std::move(other.m_data);
+    m_size = other.m_size;
+    other.m_size = 0;
+    other.m_data = nullptr;
+  }
+  // move assignment
+  Array1D<T>& operator=(Array1D<T>&& other) {
+    if (m_data) {
+      m_data.reset();
+    }
+    m_data = std::move(other.m_data);
+    m_size = other.m_size;
+    other.m_size = 0;
+    other.m_data = nullptr;
+    return *this;
   }
 
   void resize(size_t s) {
@@ -26,6 +65,18 @@ class Array1D {
       memset(m_data.get(), 0, sizeof(T) * m_size);
     }
   }
+
+  T* getData() { return m_data.get(); }
+
+  T& operator[](size_t i) { return m_data[i]; }
+  const T& operator[](size_t i) const { return m_data[i]; }
+
+  template <typename F, typename... Args>
+  void for_each(F fn, Array1D<Args>&... others) {
+    for (size_t i = 0; i < m_size; i++) {
+      m_data[i] = fn(m_data[i], others[i]...);
+    }
+  }
 };
 
 template <typename T>
@@ -36,6 +87,40 @@ class Array2D : public Array1D<T> {
  public:
   Array2D(size_t s1 = 0, size_t s2 = 0)
       : Array1D<T>(s1 * s2), m_size1(s1), m_size2(s2) {}
+
+  Array2D(const Array2D<T>& other)
+      : Array1D<T>(other), m_size1(other.m_size1), m_size2(other.m_size2) {}
+
+  // copy assignment
+  Array2D& operator=(const Array2D<T>& other) {
+    if (this != &other) {
+      Array1D<T>::operator=(other);
+      m_size1 = other.m_size1;
+      m_size2 = other.m_size2;
+    }
+    return *this;
+  }
+
+  // move constructor
+  Array2D(Array2D<T>&& other) noexcept
+      : Array1D<T>(std::move(other)),
+        m_size1(other.m_size1),
+        m_size2(other.m_size2) {
+    other.m_size1 = 0;
+    other.m_size2 = 0;
+  }
+
+  // move assignment
+  Array2D& operator=(Array2D<T>&& other) noexcept{
+    if (this != &other) {
+      Array1D<T>::operator=(std::move(other));
+      m_size1 = other.m_size1;
+      m_size2 = other.m_size2;
+      other.m_size1 = 0;
+      other.m_size2 = 0;
+    }
+    return *this;
+  }
 };
 typedef std::complex<float> Complex;
 
